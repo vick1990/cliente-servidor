43fdf9ae2416da4a4e7e3824069f3640415ce117
Vick Vasquez <vasjvic@hotmail.com>
2015-01-08 19:51:46 -0600

    Se agregaron comentarios

--- php/operaciones.php
+++ php/operaciones.php
@@ -9,11 +9,14 @@
 	}
 	class operaciones
 	{
-		
+		//Funcion constructor
 		function __construct()
 		{
+			//Se obtiene lo que nos esta llegando atraves de la variable accion y se almacena en $accion
 			$accion=$this->accion=$_REQUEST["accion"];
+			//Instanciamos la clase respueseta (creacion de un objeto)
 			$this->objeto = new respuestas($_REQUEST);
+			//Ejecutamos la funcion que se recibio atraves de la variable accion
 			$this->objeto->$accion($_REQUEST);
 		}
 	}
